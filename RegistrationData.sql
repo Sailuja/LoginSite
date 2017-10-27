604f26033b8f939d1eb80dae3d23c92bb2f7b80f
Sailuja <Sailuja@users.noreply.github.com>
2017-10-27 13:15:41 -0500

    Add files via upload

---
+++ RegistrationData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [UserName] NCHAR(20) NOT NULL, 
+    [Email] NCHAR(50) NOT NULL, 
+    [Password] NCHAR(15) NOT NULL, 
+    [Country] NCHAR(20) NOT NULL
+)
---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [UserName] NCHAR (20) NOT NULL,
+    [Email]    NCHAR (50) NOT NULL,
+    [Password] NCHAR (15) NOT NULL,
+    [Country]  NCHAR (10) NOT NULL, 
+    [ID] INT IDENTITY(1,1) NOT NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([ID])
+);
+
